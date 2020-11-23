64d6fc8718e40a39b1eedb530d834d34d2a794cc
Tania Omelko <tanyaomelko39@gmail.com>
2020-11-24 00:34:27 +0200

    Updates

---
+++ precache-manifest.6b06fd0dab21297645786c3e82e38c9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb7e210f647d0f24d9ba257125512dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7697f5aa641fa8acd43a",
+    "url": "./static/css/main.c756e45a.chunk.css"
+  },
+  {
+    "revision": "5c14782c1a185bcb563d",
+    "url": "./static/js/2.077403d6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.077403d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7697f5aa641fa8acd43a",
+    "url": "./static/js/main.8328027b.chunk.js"
+  },
+  {
+    "revision": "93b65d15acd366da5855",
+    "url": "./static/js/runtime-main.2badbef3.js"
+  }
+]);
